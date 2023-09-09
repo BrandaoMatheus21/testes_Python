df85d0cc903a7bb4ebe843589cf25a6807febbb6
matheus <matheusbrandaocorreia@gmail.com>
2023-09-09 15:08:46 -0300

    Operadores Aritmeticos

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,20 @@
+#A = 7
+#B = 2 
+#C = 4
+#D = 14
+
+A = 7
+B = 2
+C = 4
+D = 12
+
+print ( A + B - C)
+print (A * B / C)
+print (C + 22 * D)
+print (A + B * C + D)
+print ((A + B) * (C + D))
+print ((D / A) / (B / C))
+print (10 ** A)
+print (58 % A)
+print ((A + B + C * D) / (B - C * (B - C)))
+print ((A + B + C * D) / (B - C * (B - C)) + 10 ** A)
