8e191f725a17b793bf65df608e46287712ab1d80
matheus <matheusbrandaocorreia@gmail.com>
2023-09-09 14:26:31 -0300

    Temperatura

---
+++ Temperatura.py
@@ -0,0 +1,11 @@
+print ("Conversor de graus Celsius em Fahrenheit e Kelvin")
+print ("=========================================")
+
+celsius = float(input("Qual a temperatura em Celsius ?"))
+
+faren = celsius * 1,8 + 32
+Kelvin = celsius + 273
+
+
+print (f"A temperatura em Farenheit é {faren}")
+print (f"A temperatura em Kelvin é {Kelvin}")
