42831b9a238850e863964f8bfcc1f327e7c4ddd9
matheus <matheusbrandaocorreia@gmail.com>
2023-09-09 17:45:29 -0300

    Maior ou menor de idade

---
+++ AtividadeIF1.py
@@ -0,0 +1,8 @@
+#Programa para verificar maior ou menor de idade
+
+idade = int(input("Informe a sua idade:"))
+
+if idade <= 18:
+    print ("Você é menor de idade")
+elif idade >= 18:
+    print ("Você é maior de idade")
