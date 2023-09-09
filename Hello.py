83a578129997c4f647a0c879cdadc2efce4b58ed
matheus <matheusbrandaocorreia@gmail.com>
2023-09-09 10:52:13 -0300

    Meu arquivo Hello world

---
+++ Hello.py
@@ -0,0 +1,9 @@
+print ('Hello world ! ')
+
+
+nome = "Matheus"
+profissão = "estudante"
+print (f"Este é o primeiro programa de {nome}")
+print ("Eu sou", profissão, "e decidi trilhar os caminhos de Python")
+
+resposta = input("Você está pronto para vir comigo? S/N")
