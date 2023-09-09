abf615636077999c2002bac664f7bce9c1815691
matheus <matheusbrandaocorreia@gmail.com>
2023-09-09 17:44:59 -0300

    Salario com bonus

---
+++ AtividadeIF4.py
@@ -0,0 +1,8 @@
+salario = float(input("Insira seu salario"))
+tempo = float(input("Insira o seu tempo de serviço"))
+
+if tempo >= 5 :
+    print("Você possui um bônus de 5% adicional")
+
+if tempo <= 5 :
+    print ("Você não possui bõnus adicional")
