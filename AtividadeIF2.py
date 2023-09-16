ccb7384d24be9317dad88fe78f181e66999a7e8e
matheus <matheusbrandaocorreia@gmail.com>
2023-09-16 10:10:16 -0300

    Alteração

--- AtividadeIF2.py
+++ AtividadeIF2.py
@@ -3,9 +3,13 @@
 numero1 = float(input("Informe um numero"))
 numero2 = float(input("Informe mais um numero"))
 operacao = input("Informe uma operação (Divisão, adição, multiplicação e subtração)")
+resultado = ''
 
 if operacao == "Divisão": 
-    print(numero1 / numero2)
+    if numero2 == 0 :
+        print ("Nao foi possivel")
+    else:
+        print(numero1 / numero2)
 if operacao == "adição":
     print(numero1 + numero2)
 if operacao == "multiplicação":
