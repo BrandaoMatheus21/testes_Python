8cafa3b541a581cf040bc5e6afbabafcfc87dc3f
matheus <matheusbrandaocorreia@gmail.com>
2023-09-16 10:20:56 -0300

    Alteração

--- AtividadeIF3.py
+++ AtividadeIF3.py
@@ -1,11 +1,11 @@
 nota = float(input("Insira uma nota de 1 a 100:"))
 if nota >= 90 and nota <= 100:
     print ("Nota A")
-if nota >= 80 and nota <=89 :
+elif nota >= 80 and nota <=89 :
     print("Nota B")
-if nota >= 70 and nota <=79 :
+elif nota >= 70 and nota <=79 :
     print ("Nota C")
-if nota  >= 60 and nota <=69 :
+elif nota  >= 60 and nota <=69 :
     print ("Nota D")
-if nota  <= 60 :
+elif nota  <= 60 :
     print ("Nota F")
