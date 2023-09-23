2c7bfe37f8b911b408740a21d5040bde03f96db9
matheus <matheusbrandaocorreia@gmail.com>
2023-09-23 14:56:41 -0300

    alteração elif

--- Exercicio2lacos.py
+++ Exercicio2lacos.py
@@ -7,30 +7,30 @@ tabuada = int(input("Qual tabuada deseja calcular: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 if tabuada == 1:
     for numero in range (1, 11):
         print(numero * 1)
-if tabuada == 2:
+elif tabuada == 2:
     for numero in range (1, 11):
         print(numero * 2)
-if tabuada == 3:
+elif tabuada == 3:
     for numero in range (1, 11):
         print(numero * 3)
-if tabuada == 4:
+elif tabuada == 4:
     for numero in range (1,11):
         print(numero * 4)
-if tabuada == 5:
+elif tabuada == 5:
     for numero in range (1,11):
         print(numero * 5)
-if tabuada == 6:
+elif tabuada == 6:
     for numero in range (1, 11):
         print(numero * 6)
-if tabuada == 7:
+elif tabuada == 7:
     for numero in range (1, 11):
         print(numero* 7)
-if tabuada == 8:
+elif tabuada == 8:
     for numero in range (1, 11):
         print(numero * 8)
-if tabuada == 9:
+elif tabuada == 9:
     for numero in range (1, 11):
         print(numero * 9)
-if tabuada == 10:
+elif tabuada == 10:
     for numero in range (1, 11):
         print(numero * 10)
