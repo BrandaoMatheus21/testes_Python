a611ec709baad44c920d9d4f2ba546c62bfabe07
matheus <matheusbrandaocorreia@gmail.com>
2023-09-23 11:53:38 -0300

    tabuadas

---
+++ Exercicio2lacos.py
@@ -0,0 +1,36 @@
+print ("====Tabuada do 2====")
+for numero in range (1, 11):
+    print(numero * 2)
+print("===================================================================")
+
+tabuada = int(input("Qual tabuada deseja calcular: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10: "))
+if tabuada == 1:
+    for numero in range (1, 11):
+        print(numero * 1)
+if tabuada == 2:
+    for numero in range (1, 11):
+        print(numero * 2)
+if tabuada == 3:
+    for numero in range (1, 11):
+        print(numero * 3)
+if tabuada == 4:
+    for numero in range (1,11):
+        print(numero * 4)
+if tabuada == 5:
+    for numero in range (1,11):
+        print(numero * 5)
+if tabuada == 6:
+    for numero in range (1, 11):
+        print(numero * 6)
+if tabuada == 7:
+    for numero in range (1, 11):
+        print(numero* 7)
+if tabuada == 8:
+    for numero in range (1, 11):
+        print(numero * 8)
+if tabuada == 9:
+    for numero in range (1, 11):
+        print(numero * 9)
+if tabuada == 10:
+    for numero in range (1, 11):
+        print(numero * 10)
